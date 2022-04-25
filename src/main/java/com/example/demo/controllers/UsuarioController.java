3525c477a68da3c6afe1c2033e1a801491e37823
Luis <luismartinezmilla@gmail.com>
2022-04-25 12:13:23 +0200

    initial files

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping() //se ejecuta al entrar a /usuario
+    public ArrayList<UsuarioModel> obtenerListaUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping() //se ejecuta cuando recive una peticion POST
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}") //se ejecuta al entrar a /usuario/{id}
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping(path = "/query") //se ejecuta al entrar a /usuario/query?prioridad={prioridad}
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarUsuario(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if(ok){
+            return "Se eliminó el usuario con el id " + id;
+        }else{
+            return "No se pudo eliminar el usuario con el id " + id;
+        }
+    }
+}
